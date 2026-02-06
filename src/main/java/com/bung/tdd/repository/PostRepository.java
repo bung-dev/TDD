3d612a9e4829b6930bd7222f4aebf236e5bcbfb6
Bung <wndgh26@gmail.com>
2026-02-06 12:09:20 +0900

    feat: add PostRepository

---
+++ src/main/java/com/bung/tdd/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.bung.tdd.repository;
+
+import com.bung.tdd.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Long, Post> {
+}
