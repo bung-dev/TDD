ef452ac6c534ff1db361daa266ffa40c247978a2
Bung <wndgh26@gmail.com>
2026-02-06 12:39:32 +0900

    feat: add createPost

--- src/main/java/com/bung/tdd/service/PostService.java
+++ src/main/java/com/bung/tdd/service/PostService.java
@@ -1,5 +1,7 @@
 package com.bung.tdd.service;
 
+import com.bung.tdd.domain.Post;
+import com.bung.tdd.dto.PostRequest;
 import com.bung.tdd.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,15 @@ import org.springframework.stereotype.Service;
 public class PostService {
     private final PostRepository postRepository;
 
+    public Long createPost(PostRequest request){
+        if (request.title().isBlank()) {
+            throw new IllegalArgumentException("제목이 없습니다");
+        }
+        Post post = Post.builder()
+                .title(request.title())
+                .content(request.content())
+                .build();
+
+        return postRepository.save(post).getId();
+    }
 }
